240f6ea33f2dfda5f16a3c0c51ab3425100153a6
Gamma101 <danilolenberger@gmail.com>
2025-04-24 13:07:39 +0500

    made styling for login, sign up page

--- components/LoginWindow.tsx
+++ components/LoginWindow.tsx
@@ -1,15 +1,31 @@
 import React from "react"
 import { Button } from "./ui/button"
 import Link from "next/link"
+import { Input } from "./ui/input"
 
 export default function LoginWindow() {
   return (
-    <div>
-      <h1>Login Window</h1>
+    <div className="flex flex-col gap-5 dark:bg-neutral-900 bg-secondary p-10 rounded-lg">
+      <h1 className="text-2xl text-primary font-bold">Login</h1>
+      <div className="flex flex-col gap-5">
+        <div className="">
+          <p>Email</p>
+          <Input placeholder="Email" type="text" />
+        </div>
 
-      <Link href="/auth?authType=signup" replace>
-        <Button variant={"default"}>Dont Have account? Sign Up</Button>
-      </Link>
+        <div className="">
+          <p>Password</p>
+          <Input placeholder="Password" type="password" />
+        </div>
+        <Button className="text-white font-semibold">Login</Button>
+      </div>
+
+      <p>
+        Already have an account?{" "}
+        <Link className="text-primary" href="/auth?authType=signup" replace>
+          Login
+        </Link>
+      </p>
     </div>
   )
 }
--- components/SignUpWindow.tsx
+++ components/SignUpWindow.tsx
@@ -1,15 +1,49 @@
 import React from "react"
 import { Button } from "./ui/button"
 import Link from "next/link"
+import { Input } from "./ui/input"
 
 export default function SignUpWindow() {
   return (
-    <div>
-      <h1>SignUp Window</h1>
+    <div className="flex flex-col dark:bg-neutral-900 bg-secondary p-10 rounded-lg">
+      <h1 className="text-2xl text-primary font-bold">Sign Up</h1>
+      <div className="flex flex-col gap-5">
+        <div className="">
+          <p>Email</p>
+          <Input placeholder="Email" type="text" />
+        </div>
 
-      <Link href="/auth?authType=login" replace>
-        <Button variant={"default"}>Already have an account? login</Button>
-      </Link>
+        <div className="">
+          <p>Password</p>
+          <Input placeholder="Password" type="password" />
+        </div>
+        <div className="">
+          <p>Confirm Password</p>
+          <Input placeholder="Confirm Password" type="password" />
+        </div>
+        <Button className="text-white font-semibold">Sign Up</Button>
+      </div>
+
+      <p className="pt-5">
+        Already have an account?
+        <Link className="text-primary" href="/auth?authType=login" replace>
+          Login
+        </Link>
+      </p>
+      <div className="flex flex-col gap-3 py-5">
+        <Button
+          className="dark:bg-secondary bg-white text-black dark:text-white"
+          variant={"secondary"}
+        >
+          use Github
+        </Button>
+        <Button
+          className="dark:bg-secondary bg-white text-black dark:text-white"
+          variant={"secondary"}
+        >
+          use Google
+        </Button>
+      </div>
     </div>
   )
 }
